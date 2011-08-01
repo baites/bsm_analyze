152f8209b5cbf49268cc90c7fa22ae907f987bf5
Samvel Khalatyan <samvel.khalatyan@gmail.com>
2011-08-01 15:46:05 -0500

    Add synch selector options; implement synch v3

--- interface/Selector.h
+++ interface/Selector.h
@@ -17,6 +17,7 @@
 #include "bsm_core/interface/Object.h"
 #include "bsm_input/interface/bsm_input_fwd.h"
 #include "JetMETObjects/interface/JetCorrectorParameters.h"
+#include "interface/AppController.h"
 #include "interface/Cut.h"
 
 namespace bsm
@@ -248,7 +249,51 @@ namespace bsm
 
     // Synchronization Exercise Selector
     //
-    class SynchSelector : public Selector
+    class SynchSelectorDelegate
+    {
+        public:
+            enum LeptonMode
+            {
+                ELECTRON = 0,
+                MUON
+            };
+
+            enum CutMode
+            {
+                CUT_2D = 0,
+                ISOLATION
+            };
+
+            virtual ~SynchSelectorDelegate() {}
+
+            virtual void setLeptonMode(const LeptonMode &) {}
+            virtual void setCutMode(const CutMode &) {}
+    };
+
+    class SynchSelectorOptions : public Options
+    {
+        public:
+            SynchSelectorOptions();
+            virtual ~SynchSelectorOptions();
+
+            // Options interface
+            //
+            virtual OptionsPtr options() const;
+
+            void setLeptonMode(std::string);
+            void setCutMode(std::string);
+
+            void setDelegate(SynchSelectorDelegate *);
+            SynchSelectorDelegate *delegate() const;
+
+        private:
+            SynchSelectorDelegate *_delegate;
+
+            OptionsPtr _options;
+    };
+
+    class SynchSelector : public Selector,
+        public SynchSelectorDelegate
     {
         public:
             typedef boost::shared_ptr<MultiplicityCutflow> CutflowPtr;
@@ -270,18 +315,6 @@ namespace bsm
                 SELECTIONS // this item should always be the last one
             };
 
-            enum LeptonMode
-            {
-                ELECTRON = 0,
-                MUON
-            };
-
-            enum CutMode
-            {
-                CUT_2D = 0,
-                ISOLATION
-            };
-
             SynchSelector(const LeptonMode & = ELECTRON,
                     const CutMode & = CUT_2D);
             SynchSelector(const SynchSelector &);
@@ -289,6 +322,15 @@ namespace bsm
             // Test if muon passes the selector
             //
             virtual bool apply(const Event *);
+
+            CutflowPtr cutflow() const;
+
+            JetEnergyCorrectionDelegate *getJetEnergyCorrectionDelegate() const;
+
+            // SynchSelectorDelegate interface
+            //
+            virtual void setLeptonMode(const LeptonMode &);
+            virtual void setCutMode(const CutMode &);
 
             // Selector interface
             //
@@ -304,26 +346,47 @@ namespace bsm
 
             virtual void print(std::ostream &) const;
 
-            // Selection Cutflow steps
-            //
-            void setJetEnergyCorrections(const Corrections &corrections);
-            CutflowPtr cutflow() const;
-
         private:
-            // Prevent copying
-            //
-            SynchSelector &operator =(const SynchSelector &);
+            struct CorrectedJet
+            {
+                CorrectedJet()
+                {
+                    jet = 0;
+                }
+
+                const Jet *jet;
+                LorentzVector corrected_p4;
+            };
+
+            typedef std::vector<const Electron *> GoodElectrons;
+            typedef std::vector<const Muon *> GoodMuons;
+            typedef std::vector<CorrectedJet> GoodJets;
 
             bool primaryVertices(const Event *);
+            bool jets(const Event *);
+            bool lepton(const Event *);
+            bool secondaryLeptonVeto(const Event *);
+
+            void selectGoodElectrons(const Event *);
+            void selectGoodMuons(const Event *);
 
             LeptonMode _lepton_mode;
             CutMode _cut_mode;
 
-            // Selectors
-            //
-            boost::shared_ptr<PrimaryVertexSelector> _primary_vertex_selector;
-
             CutflowPtr _cutflow;
+
+            boost::shared_ptr<PrimaryVertexSelector> _primary_vertex_selector;
+            boost::shared_ptr<ElectronSelector> _electron_selector;
+            boost::shared_ptr<MuonSelector> _muon_selector;
+            boost::shared_ptr<JetSelector> _nice_jet_selector;
+            boost::shared_ptr<JetSelector> _good_jet_selector;
+
+            boost::shared_ptr<JetEnergyCorrections> _jec;
+
+            GoodElectrons _good_electrons;
+            GoodMuons _good_muons;
+            GoodJets _nice_jets; // pT > 25
+            GoodJets _good_jets; // pT > 50
     };
 
     class WJetSelector : public Selector
--- src/Selector.cc
+++ src/Selector.cc
@@ -11,6 +11,7 @@
 #include <ostream>
 #include <sstream>
 
+#include <boost/algorithm/string.hpp>
 #include <boost/pointer_cast.hpp>
 
 #include "bsm_core/interface/ID.h"
@@ -22,19 +23,24 @@
 #include "bsm_input/interface/Physics.pb.h"
 #include "bsm_input/interface/PrimaryVertex.pb.h"
 #include "interface/Cut.h"
+#include "interface/JetEnergyCorrections.h"
 #include "interface/Selector.h"
 #include "interface/Utility.h"
 
 using namespace std;
 
 using boost::dynamic_pointer_cast;
+using boost::to_lower;
 
 using bsm::CutPtr;
 using bsm::ElectronSelector;
+using bsm::JetEnergyCorrectionDelegate;
 using bsm::JetSelector;
 using bsm::MultiplicityCutflow;
 using bsm::MuonSelector;
 using bsm::PrimaryVertexSelector;
+using bsm::SynchSelectorDelegate;
+using bsm::SynchSelectorOptions;
 using bsm::SynchSelector;
 using bsm::WJetSelector;
 using bsm::LockSelectorEventCounterOnUpdate;
@@ -553,6 +559,78 @@ void PrimaryVertexSelector::print(std::ostream &out) const
 
 
 
+// Synch Selector Options
+//
+SynchSelectorOptions::SynchSelectorOptions()
+{
+    _delegate = 0;
+
+    _options.reset(new po::options_description("Jet Energy Correction Options"));
+    _options->add_options()
+        ("lepton-mode",
+         po::value<string>()->notifier(
+             boost::bind(&SynchSelectorOptions::setLeptonMode, this, _1)),
+         "synchronization selector lepton mode: muon, electron")
+
+        ("cut-mode",
+         po::value<string>()->notifier(
+             boost::bind(&SynchSelectorOptions::setCutMode, this, _1)),
+         "synchroniation selector cut mode: 2dcut, isolation")
+    ;
+}
+
+SynchSelectorOptions::~SynchSelectorOptions()
+{
+}
+
+SynchSelectorOptions::OptionsPtr SynchSelectorOptions::options() const
+{
+    return _options;
+}
+
+void SynchSelectorOptions::setLeptonMode(std::string mode)
+{
+    if (!_delegate)
+        return;
+
+    to_lower(mode);
+
+    if ("electron" == mode)
+        _delegate->setLeptonMode(SynchSelectorDelegate::ELECTRON);
+    else if ("muon" == mode)
+        _delegate->setLeptonMode(SynchSelectorDelegate::MUON);
+    else
+        cerr << "unsupported synchronization selector lepton mode" << endl;
+}
+
+void SynchSelectorOptions::setCutMode(std::string mode)
+{
+    if (!_delegate)
+        return;
+
+    to_lower(mode);
+
+    if ("2dcut" == mode)
+        _delegate->setCutMode(SynchSelectorDelegate::CUT_2D);
+    else if ("isolation" == mode)
+        _delegate->setCutMode(SynchSelectorDelegate::ISOLATION);
+    else
+        cerr << "unsupported synchronization selector cut mode" << endl;
+}
+
+void SynchSelectorOptions::setDelegate(SynchSelectorDelegate *delegate)
+{
+    if (_delegate != delegate)
+        _delegate = delegate;
+}
+
+SynchSelectorDelegate *SynchSelectorOptions::delegate() const
+{
+    return _delegate;
+}
+
+
+
 // Synchronization Exercise Selector
 //
 SynchSelector::SynchSelector(const LeptonMode &lepton_mode,
@@ -563,23 +641,6 @@ SynchSelector::SynchSelector(const LeptonMode &lepton_mode,
     // Cutflow table
     //
     _cutflow.reset(new MultiplicityCutflow(SELECTIONS - 1));
-    _cutflow->cut(PRESELECTION)->setName("pre-selection");
-    _cutflow->cut(SCRAPING)->setName("Scraping Veto");
-    _cutflow->cut(HBHENOISE)->setName("HBHE Noise");
-    _cutflow->cut(PRIMARY_VERTEX)->setName("Good Primary Vertex");
-    _cutflow->cut(JET)->setName("2 Good Jets");
-
-    ostringstream lepton;
-    lepton << _lepton_mode;
-
-    _cutflow->cut(LEPTON)->setName(string("Good ") + lepton.str());
-    _cutflow->cut(VETO_SECOND_LEPTON)->setName("Veto 2nd lepton");
-
-    lepton << " " << _cut_mode;
-    _cutflow->cut(CUT_LEPTON)->setName(lepton.str());
-
-    _cutflow->cut(LEADING_JET)->setName("Leading Jet");
-    _cutflow->cut(HTLEP)->setName("hTlep");
     monitor(_cutflow);
 
     // Selectors
@@ -587,6 +648,30 @@ SynchSelector::SynchSelector(const LeptonMode &lepton_mode,
     _primary_vertex_selector.reset(new PrimaryVertexSelector());
     _primary_vertex_selector->rho()->setValue(2.0);
     monitor(_primary_vertex_selector);
+
+    _electron_selector.reset(new ElectronSelector());
+    _electron_selector->primary_vertex()->disable();
+    monitor(_electron_selector);
+
+    _muon_selector.reset(new MuonSelector());
+    _muon_selector->pt()->setValue(35);
+    monitor(_muon_selector);
+
+    // Nice jets have pT > 25
+    //
+    _nice_jet_selector.reset(new JetSelector());
+    _nice_jet_selector->pt()->setValue(25);
+    monitor(_nice_jet_selector);
+
+    // Good jets have pT > 50
+    //
+    _good_jet_selector.reset(new JetSelector());
+    monitor(_good_jet_selector);
+
+    // Jet Energy Corrections
+    //
+    _jec.reset(new JetEnergyCorrections());
+    monitor(_jec);
 }
 
 SynchSelector::SynchSelector(const SynchSelector &object):
@@ -606,13 +691,66 @@ SynchSelector::SynchSelector(const SynchSelector &object):
         dynamic_pointer_cast<PrimaryVertexSelector>(object._primary_vertex_selector->clone());
 
     monitor(_primary_vertex_selector);
+
+    _electron_selector = 
+        dynamic_pointer_cast<ElectronSelector>(object._electron_selector->clone());
+
+    monitor(_electron_selector);
+
+    _muon_selector = 
+        dynamic_pointer_cast<MuonSelector>(object._muon_selector->clone());
+
+    monitor(_muon_selector);
+
+    _nice_jet_selector = 
+        dynamic_pointer_cast<JetSelector>(object._nice_jet_selector->clone());
+
+    monitor(_nice_jet_selector);
+
+    _good_jet_selector = 
+        dynamic_pointer_cast<JetSelector>(object._good_jet_selector->clone());
+
+    monitor(_good_jet_selector);
+
+    // Jet Energy Corrections
+    //
+    _jec = dynamic_pointer_cast<JetEnergyCorrections>(object._jec->clone());
+    monitor(_jec);
 }
 
 bool SynchSelector::apply(const Event *event)
 {
     _cutflow->apply(PRESELECTION);
 
-    return primaryVertices(event);
+    _good_electrons.clear();
+    _good_muons.clear();
+    _nice_jets.clear();
+    _good_jets.clear();
+
+    return primaryVertices(event)
+        && jets(event)
+        && lepton(event)
+        && secondaryLeptonVeto(event);
+}
+
+JetEnergyCorrectionDelegate *SynchSelector::getJetEnergyCorrectionDelegate() const
+{
+    return _jec.get();
+}
+
+void SynchSelector::setLeptonMode(const LeptonMode &lepton_mode)
+{
+    _lepton_mode = lepton_mode;
+}
+
+void SynchSelector::setCutMode(const CutMode &cut_mode)
+{
+    _cut_mode = cut_mode;
+}
+
+SynchSelector::CutflowPtr SynchSelector::cutflow() const
+{
+    return _cutflow;
 }
 
 void SynchSelector::enable()
@@ -637,6 +775,24 @@ SynchSelector::ObjectPtr SynchSelector::clone() const
 
 void SynchSelector::print(std::ostream &out) const
 {
+    _cutflow->cut(PRESELECTION)->setName("pre-selection");
+    _cutflow->cut(SCRAPING)->setName("Scraping Veto");
+    _cutflow->cut(HBHENOISE)->setName("HBHE Noise");
+    _cutflow->cut(PRIMARY_VERTEX)->setName("Good Primary Vertex");
+    _cutflow->cut(JET)->setName("2 Good Jets");
+
+    ostringstream lepton;
+    lepton << _lepton_mode;
+
+    _cutflow->cut(LEPTON)->setName(string("Good ") + lepton.str());
+    _cutflow->cut(VETO_SECOND_LEPTON)->setName("Veto 2nd lepton");
+
+    lepton << " " << _cut_mode;
+    _cutflow->cut(CUT_LEPTON)->setName(lepton.str());
+
+    _cutflow->cut(LEADING_JET)->setName("Leading Jet");
+    _cutflow->cut(HTLEP)->setName("hTlep");
+
     out << "Cutflow [" << _lepton_mode << ": " << _cut_mode << "]" << endl;
     out << *_cutflow << endl;
     out << endl;
@@ -649,6 +805,107 @@ bool SynchSelector::primaryVertices(const Event *event)
     return event->primary_vertices().size()
         && _primary_vertex_selector->apply(*event->primary_vertices().begin())
         && (_cutflow->apply(PRIMARY_VERTEX), true);
+}
+
+bool SynchSelector::jets(const Event *event)
+{
+    selectGoodElectrons(event);
+    selectGoodMuons(event);
+
+    typedef ::google::protobuf::RepeatedPtrField<Jet> Jets;
+
+    LockSelectorEventCounterOnUpdate lock_nice_jets(*_nice_jet_selector);
+    LockSelectorEventCounterOnUpdate lock_good_jets(*_good_jet_selector);
+    for(Jets::const_iterator jet = event->jets().begin();
+            event->jets().end() != jet;
+            ++jet)
+    {
+        LorentzVector corrected_p4 = _jec->correctJet(&*jet,
+                event,
+                _good_electrons,
+                _good_muons);
+
+        // Original jet in the event can not be modified and Jet Selector can
+        // only be applied to jet: therefore copy jet, set corrected p4 and
+        // apply selector
+        //
+        Jet corrected_jet = *jet;
+        *corrected_jet.mutable_physics_object()->mutable_p4() = corrected_p4;
+
+        if (!_nice_jet_selector->apply(corrected_jet))
+            continue;
+
+        // Store original jet and corrected p4
+        //
+        CorrectedJet tmp;
+        tmp.jet = &*jet;
+        tmp.corrected_p4 = corrected_p4;
+
+        _nice_jets.push_back(tmp);
+
+        if (!_good_jet_selector->apply(corrected_jet))
+            continue;
+
+        _good_jets.push_back(tmp);
+    }
+
+    return 1 < _good_jets.size()
+        && (_cutflow->apply(JET), true);
+}
+
+bool SynchSelector::lepton(const Event *event)
+{
+    return (ELECTRON == _lepton_mode
+        ? !_good_electrons.empty()
+        : !_good_muons.empty())
+
+        && (_cutflow->apply(LEPTON), true);
+}
+
+bool SynchSelector::secondaryLeptonVeto(const Event *event)
+{
+    return (ELECTRON == _lepton_mode
+        ? (1 == _good_electrons.size()
+            && _good_muons.empty())
+
+        : (1 == _good_muons.size()
+            && _good_electrons.empty()))
+
+        && (_cutflow->apply(VETO_SECOND_LEPTON), true);
+}
+
+void SynchSelector::selectGoodElectrons(const Event *event)
+{
+    _good_electrons.clear();
+
+    typedef ::google::protobuf::RepeatedPtrField<Electron> Electrons;
+
+    const PrimaryVertex &pv = *event->primary_vertices().begin();
+
+    LockSelectorEventCounterOnUpdate electron_lock(*_electron_selector);
+    for(Electrons::const_iterator electron = event->pf_electrons().begin();
+            event->pf_electrons().end() != electron;
+            ++electron)
+    {
+        if (_electron_selector->apply(*electron, pv))
+            _good_electrons.push_back(&*electron);
+    }
+}
+
+void SynchSelector::selectGoodMuons(const Event *event)
+{
+    typedef ::google::protobuf::RepeatedPtrField<Muon> Muons;
+
+    const PrimaryVertex &pv = *event->primary_vertices().begin();
+
+    LockSelectorEventCounterOnUpdate muon_lock(*_muon_selector);
+    for(Muons::const_iterator muon = event->pf_muons().begin();
+            event->pf_muons().end() != muon;
+            ++muon)
+    {
+        if (_muon_selector->apply(*muon, pv))
+            _good_muons.push_back(&*muon);
+    }
 }
 
 
@@ -844,10 +1101,10 @@ std::ostream &bsm::operator <<(std::ostream &out,
 {
     switch(lepton_mode)
     {
-        case SynchSelector::ELECTRON:   out << "electron";
+        case SynchSelector::ELECTRON:   out << "Electron";
                                         break;
 
-        case SynchSelector::MUON:       out << "muon";
+        case SynchSelector::MUON:       out << "Muon";
                                         break;
 
         default:                        out << "unknown";
