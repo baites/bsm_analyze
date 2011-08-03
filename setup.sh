cc9802b13e65e28300ad6416162771bb526086ff
Samvel Khalatyan <samvel.khalatyan@gmail.com>
2011-08-03 14:58:56 -0500

    Adjust the setup.sh script to Linux/OSX

--- setup.sh
+++ setup.sh
@@ -1,17 +1,22 @@
 #!/usr/bin/env bash
 
 CODE=`pwd`
-cmd="echo $DYLD_LIBRARY_PATH | awk '{print index(\$0, \"${CODE}\")}'"
+
+library_path=DYLD_LIBRARY_PATH
+if [[ "Linux" == "`uname`" ]]
+then
+    library_path=LD_LIBRARY_PATH
+fi
+
+cmd="echo \$${library_path} | awk '{print index(\$0, \"${CODE}\")}'"
 index=`eval $cmd`
 
 if [[ "0" -ne "$index" ]]
 then
     echo "Environment is already set."
 else
-    echo "Setting environment."
+    echo "setting the environment"
 
-    export DYLD_LIBRARY_PATH="${DYLD_LIBRARY_PATH}:${CODE}/lib"
-    export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${CODE}/lib"
-
+    export ${library_path}="${!library_path}:${CODE}/lib"
     export PATH=${CODE}/bin:${PATH}
 fi
