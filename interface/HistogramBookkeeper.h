5833b350c358cbcbb02707d9cc67e22b5348c49c
Samvel Khalatyan <samvel.khalatyan@gmail.com>
2011-08-23 15:22:20 -0500

    Add Histogram Bookkeper from Victor

---
+++ interface/HistogramBookkeeper.h
@@ -0,0 +1,92 @@
+// Analyzer: HistogramProducer
+//
+// Generate a series of histograms based on PB.
+//
+// Created by Victor E. Bazterra, August 3th 2011 UIC
+//
+
+#ifndef BSM_HISTOGRAM_BOOKKEEPER
+#define BSM_HISTOGRAM_BOOKKEEPER
+
+#include <boost/shared_ptr.hpp>
+#include <boost/unordered_map.hpp>
+
+#include "bsm_core/interface/ID.h"
+#include "bsm_core/interface/Object.h"
+#include "bsm_stat/interface/H1.h"
+#include "bsm_stat/interface/H2.h"
+#include "interface/StatProxy.h"
+
+namespace bsm
+{
+
+
+class HistogramBookkeeper : public core::Object
+{
+public:
+
+    typedef boost::shared_ptr<stat::H1> H1Ptr;
+    typedef boost::shared_ptr<stat::H2> H2Ptr;
+    typedef boost::shared_ptr<H1Proxy> H1ProxyPtr;
+    typedef boost::shared_ptr<H2Proxy> H2ProxyPtr;
+    typedef boost::unordered_map<std::string, H1ProxyPtr> H1ProxyPtrContainer;
+    typedef boost::unordered_map<std::string, H2ProxyPtr> H2ProxyPtrContainer;
+
+    HistogramBookkeeper() {};
+    HistogramBookkeeper(HistogramBookkeeper const & object);
+
+    // Booking 1d histograms
+    void book1d(std::string const & key,
+                uint32_t const & bins, float const & min, float const & max
+               )
+    {
+        _cache1d[key].reset(new H1Proxy(bins, min, max));
+    }
+
+    // Booking 2d histograms
+    void book2d(std::string const & key,
+                uint32_t const & xbins, float const & xmin, float const & xmax,
+                uint32_t const & ybins, float const & ymin, float const & ymax
+               )
+    {
+        _cache2d[key].reset(new H2Proxy(xbins, xmin, xmax, ybins, ymin, ymax));
+    }
+
+    // Get a pointer 1d histograms
+    const H1Ptr get1d(std::string const & key)
+    {
+        return _cache1d.at(key)->histogram();
+    }
+
+    // Get a pointer 1d histograms
+    const H2Ptr get2d(std::string const & key)
+    {
+        return _cache2d.at(key)->histogram();
+    }
+
+    virtual void print(std::ostream &) const;
+
+    void write(std::string const &);
+
+    virtual uint32_t id() const
+    {
+        return core::ID<HistogramBookkeeper>::get();
+    }
+
+    virtual ObjectPtr clone() const
+    {
+        return ObjectPtr(new HistogramBookkeeper(*this));
+    }
+
+    void merge(const ObjectPtr &);
+
+private:
+
+    H1ProxyPtrContainer _cache1d;
+    H2ProxyPtrContainer _cache2d;
+
+};
+
+}
+
+#endif
---
+++ src/HistogramBookkeeper.cc
@@ -0,0 +1,155 @@
+
+#include <algorithm>
+#include <string>
+
+#include <boost/format.hpp>
+
+#include <TH1D.h>
+#include <TH2D.h>
+#include <TFile.h>
+
+#include <bsm_stat/interface/Bin.h>
+#include <bsm_stat/interface/Utility.h>
+#include <interface/HistogramBookkeeper.h>
+
+namespace bsm
+{
+
+using boost::format;
+using namespace std;
+using namespace stat;
+
+static TH1Ptr convert(string const & name, H1 const & h1)
+{
+    TH1Ptr h(new TH1D());
+
+    h->SetName(name.c_str());
+    h->Sumw2();
+
+    if (h1)
+    {
+        h->SetBins(h1.axis()->bins(), h1.axis()->min(), h1.axis()->max());
+
+        for(int bin = 0, bins = 1 + h1.axis()->bins(); bins >= bin; ++bin)
+        {
+            const Bin & b(h1[bin]);
+            h->SetBinContent(bin, b.data());
+            h->SetBinError(bin, b.error());
+        }
+
+        h->SetEntries(h1.entries());
+    }
+
+    return h;
+}
+
+
+static TH2Ptr convert(string const & name, const H2 &h2)
+{
+    TH2Ptr h(new TH2D());
+
+    h->SetName(name.c_str());
+    h->Sumw2();
+
+    if (!h2)
+        return h;
+
+    h->SetBins(
+        h2.xAxis()->bins(), h2.xAxis()->min(),h2.xAxis()->max(),
+        h2.yAxis()->bins(), h2.yAxis()->min(),h2.yAxis()->max()
+    );
+
+    for(
+        uint32_t x_bin = 0,
+        x_bins = 1 + h2.xAxis()->bins(),
+        y_bins = 1 + h2.yAxis()->bins();
+        x_bins >= x_bin;
+        ++x_bin
+    )
+        for(uint32_t y_bin = 0; y_bins >= y_bin; ++y_bin)
+        {
+            const Bin & b(h2[x_bin][y_bin]);
+            h->SetBinContent(x_bin, y_bin, b.data());
+            h->SetBinError(x_bin, y_bin, b.error());
+        }
+
+    h->SetEntries(h2.entries());
+    return h;
+}
+
+
+HistogramBookkeeper::HistogramBookkeeper(HistogramBookkeeper const & object)
+{
+    for ( H1ProxyPtrContainer::const_iterator h1 = object._cache1d.begin(); h1 != object._cache1d.end(); ++h1 )
+        _cache1d[h1->first].reset(new H1Proxy(*h1->second));
+    for ( H2ProxyPtrContainer::const_iterator h2 = object._cache2d.begin(); h2 != object._cache2d.end(); ++h2 )
+        _cache2d[h2->first].reset(new H2Proxy(*h2->second));
+}
+
+
+void HistogramBookkeeper::merge(const ObjectPtr & pointer)
+{
+    if (id() != pointer->id())
+        return;
+
+    boost::shared_ptr<HistogramBookkeeper> object =
+        boost::dynamic_pointer_cast<HistogramBookkeeper>(pointer);
+
+    if (!object)
+        return;
+
+    for ( H1ProxyPtrContainer::const_iterator h1 = object->_cache1d.begin(); h1 != object->_cache1d.end(); ++h1 )
+        _cache1d[h1->first]->merge(h1->second);
+    for ( H2ProxyPtrContainer::const_iterator h2 = object->_cache2d.begin(); h2 != object->_cache2d.end(); ++h2 )
+        _cache2d[h2->first]->merge(h2->second);
+}
+
+
+void HistogramBookkeeper::write(std::string const & filename)
+{
+    boost::shared_ptr<TFile> file = boost::shared_ptr<TFile>(new TFile(filename.c_str(),"recreate"));
+
+    for ( H1ProxyPtrContainer::const_iterator h1 = _cache1d.begin(); h1 != _cache1d.end(); ++h1 )
+    {
+        TH1Ptr th1 = convert(h1->first, (*h1->second->histogram()));
+        th1->Write();
+    }
+    for ( H2ProxyPtrContainer::const_iterator h2 = _cache2d.begin(); h2 != _cache2d.end(); ++h2 )
+    {
+        TH2Ptr th2 = convert(h2->first, (*h2->second->histogram()));
+        th2->Write();
+    }
+}
+
+
+void HistogramBookkeeper::print(std::ostream & os) const
+{
+    vector<string> keys;
+
+    for ( H1ProxyPtrContainer::const_iterator h1 = _cache1d.begin(); h1 != _cache1d.end(); ++h1 )
+        keys.push_back(h1->first);
+
+    sort(keys.begin(), keys.end());
+
+    for (vector<string>::const_iterator key = keys.begin(); key != keys.end(); ++key)
+        os << format ("%-30s : %7d\n") % (*key) % _cache1d.at(*key)->histogram()->entries();
+
+    os << "\n";
+
+    os << format("Number of 2d histograms: %d\n") % _cache2d.size();
+    os << "========================================\n";
+
+    keys.clear();
+
+    for ( H2ProxyPtrContainer::const_iterator h2 = _cache2d.begin(); h2 != _cache2d.end(); ++h2 )
+        keys.push_back(h2->first);
+
+    sort(keys.begin(), keys.end());
+
+    for (vector<string>::const_iterator key = keys.begin(); key != keys.end(); ++key)
+        os << format ("%-30s : %7d\n") % (*key) % _cache1d.at(*key)->histogram()->entries();
+}
+
+
+}
+
