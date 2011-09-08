5f1cbadfbc55b06db47cc8aa90238a2ac2fda89e
Samvel Khalatyan <samvel.khalatyan@gmail.com>
2011-09-08 14:34:18 -0500

    Cache subtracted p4 in JEC

--- src/JetEnergyCorrections.cc
+++ src/JetEnergyCorrections.cc
@@ -201,6 +201,9 @@ JetEnergyCorrections::CorrectedJet JetEnergyCorrections::correctJet(
         }
     }
 
+    corrected_jet.subtracted_p4.reset(new LorentzVector());
+    corrected_jet.subtracted_p4->CopyFrom(*corrected_jet.corrected_p4);
+
     correct(corrected_jet, event);
 
     return corrected_jet;
