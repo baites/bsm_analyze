db295e230cf0b2cde78dda5b932ce5368f61c2e3
Samvel Khalatyan <samvel.khalatyan@gmail.com>
2011-10-24 19:37:45 -0500

    Add missing CorrectedJet class

---
+++ interface/CorrectedJet.h
@@ -0,0 +1,57 @@
+// Corrected jet structure: reference to the original jet, subtracted leptons,
+// p4 after subtractions and corrected p4.
+//
+// Created by Samvel Khalatyan, Jul 31, 2011
+// Copyright 2011, All rights reserved
+
+#ifndef BSM_CORRECTED_JET
+#define BSM_CORRECTED_JET
+
+#include <vector>
+
+#include <boost/shared_ptr.hpp>
+
+#include "bsm_input/interface/bsm_input_fwd.h"
+
+namespace bsm
+{
+    struct CorrectedJet
+    {
+        typedef boost::shared_ptr<LorentzVector> LorentzVectorPtr;
+
+        typedef std::vector<const Electron *> Electrons;
+        typedef std::vector<const Muon *> Muons;
+
+        CorrectedJet():
+            correction(0)
+        {
+            jet = 0;
+        }
+
+        // reference to the origal jet
+        //
+        const Jet *jet;
+
+        // Corrected jet p4
+        //
+        LorentzVectorPtr corrected_p4;
+
+        // jet uncorrected p4 aftre leptons subtraction
+        //
+        LorentzVectorPtr subtracted_p4;
+
+        // electrons that were subtracted
+        //
+        Electrons subtracted_electrons;
+
+        // muons that were subtracted
+        //
+        Muons subtracted_muons;
+
+        // Jet energy correction that was applied
+        //
+        float correction;
+    };
+}
+
+#endif
