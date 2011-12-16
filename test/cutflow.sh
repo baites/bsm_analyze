d8ae8b54c0bdec95046919fcbb3c3a77e21c0c90
Samvel Khalatyan <samvel.khalatyan@gmail.com>
2011-12-16 16:09:30 -0600

    Add qcd_data to cutflow

--- test/cutflow.sh
+++ test/cutflow.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
+filename=debug_signal_p150_hlt.log
 sum_samples()
 {
     cuts=5
     for folder in $@
     do
-        tail -30 $folder/debug_signal_p150_hlt.log | grep "[+]" | sed 's/^.*= //' | awk '{print $3}' | grep -E '[0-9]' | tail -$cuts | xargs -I {} echo {}\ | xargs echo | sed 's/ $/\n/'
+        tail -30 $folder/$filename | grep "[+]" | sed 's/^.*= //' | awk '{print $3}' | grep -E '[0-9]' | tail -$cuts | xargs -I {} echo {}\ | xargs echo | sed 's/ $/\n/'
     done | awk '{a+=$1; b+=$2; c+=$3; d+=$4; e+=$5; f+=$6} END { print a, b, c, d, e, f}'
 }
 
@@ -14,6 +15,12 @@ do
     echo -n "$folder "
     sum_samples $folder
 done
+
+filename=debug_signal_p150_qcd.log
+
+echo -n "qcd_data "
+sum_samples golden_*
+filename=debug_signal_p150_hlt.log
 
 echo -n "data "
 sum_samples golden_*
