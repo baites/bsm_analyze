7c0c58c66f8602ecf0f1e73328e1b3f2f321b469
Samvel Khalatyan <samvel.khalatyan@gmail.com>
2011-12-20 17:46:12 -0600

    Reverse MET, tricuts for QCD from data cutflow

--- test/cutflow_sum.sh
+++ test/cutflow_sum.sh
@@ -5,10 +5,21 @@ sum_samples()
 {
     for pattern in $@
     do
-        grep -E "^$pattern" $filename
-    #done | awk '{a+=$2; b+=$3; c+=$4; d+=$5; e+=$6; f+=$7} END {print a, "\\pm", sqrt(a), "&", b, "&", c, "&", d, "&", e, "\\\\"}'
-done | awk '{a+=$2; b+=$3; c+=$4; d+=$5; e+=$6; f+=$7} END {printf("\$%.2f \\pm %.2f\$ & \$%.2f \\pm %.2f\$ & \$%.2f \\pm %.2f\$ & \$%.2f \\pm %.2f\$ & \$%.2f \\pm %.2f\$ \\\\\n", a, sqrt(a), b, sqrt(b), c, sqrt(c), d, sqrt(d), e, sqrt(e))}'
+        if [[ "qcd_data" != "$pattern" ]]
+        then
+            grep -E "^$pattern" $filename
+        else
+            echo `grep -E "^$pattern" $filename | awk 'END {print $1, $2, $3, $4, $6, $5, $7}'`
+        fi
+    done | awk '{a+=$2; b+=$3; c+=$4; d+=$5; e+=$6; f+=$7} END {printf("\$%.2f \\pm %.2f\$ & \$%.2f \\pm %.2f\$ & \$%.2f \\pm %.2f\$ & \$%.2f \\pm %.2f\$ & \$%.2f \\pm %.2f\$ \\\\\n", a, sqrt(a), b, sqrt(b), c, sqrt(c), d, sqrt(d), e, sqrt(e))}'
 }
+
+for input in zprime_m{1,2,3}*
+do
+    echo -n "$input & "
+    sum_samples "$input"
+done
+echo \\hline
 
 echo -n "stop & "
 sum_samples "stop" satop
@@ -19,19 +30,20 @@ do
     sum_samples "$input"
 done
 
+echo \\hline
+
 echo -n "total & "
 sum_samples "stop" satop {w,z,tt}jets
 
 echo -n "qcd_data & "
 sum_samples qcd_data
 
+echo -n "total background & "
+sum_samples "stop" satop {w,z,tt}jets qcd_data
+
 echo -n "data & "
 sum_samples data
 
-for input in zprime_m{1,2,3}*
-do
-    echo -n "$input & "
-    sum_samples "$input"
-done
+echo \\hline
 
 exit 0
