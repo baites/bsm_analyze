3be9a8a120ac648318ab465954b344b268d22b12
Samvel Khalatyan <samvel.khalatyan@gmail.com>
2012-01-05 09:45:21 -0600

    Remove errors from data in cutflow table

--- test/cutflow_sum.sh
+++ test/cutflow_sum.sh
@@ -11,38 +11,82 @@ sum_samples()
         else
             echo `grep -E "^$pattern" $filename | awk 'END {print $1, $2, $3, $4, $6, $5, $7}'`
         fi
-    done | awk '{a+=$2; b+=$3; c+=$4; d+=$5; e+=$6; f+=$7} END {printf("\$%.2f \\pm %.2f\$ & \$%.2f \\pm %.2f\$ & \$%.2f \\pm %.2f\$ & \$%.2f \\pm %.2f\$ & \$%.2f \\pm %.2f\$ \\\\\n", a, sqrt(a), b, sqrt(b), c, sqrt(c), d, sqrt(d), e, sqrt(e))}'
+    done | awk '{a+=$2; b+=$3; c+=$4; d+=$5; e+=$6; f+=$7} END {printf("\$%.0f \\pm %.0f\$ & \$%.0f \\pm %.0f\$ & \$%.0f \\pm %.0f\$ & \$%.0f \\pm %.0f\$ & \$%.0f \\pm %.0f\$ \\\\\n", a, sqrt(a), b, sqrt(b), c, sqrt(c), d, sqrt(d), e, sqrt(e))}'
+}
+
+sum_samples_no_error()
+{
+    for pattern in $@
+    do
+        if [[ "qcd_data" != "$pattern" ]]
+        then
+            grep -E "^$pattern" $filename
+        else
+            echo `grep -E "^$pattern" $filename | awk 'END {print $1, $2, $3, $4, $6, $5, $7}'`
+        fi
+    done | awk '{a+=$2; b+=$3; c+=$4; d+=$5; e+=$6; f+=$7} END {printf("\$%.0f\$ & \$%.0f\$ & \$%.0f\$ & \$%.0f\$ & \$%.0f\$ \\\\\n", a, b, c, d, e)}'
 }
 
 for input in zprime_m{1,2,3}*
 do
-    echo -n "$input & "
+    echo -n "\$Z^\\prime\$, \$M="
+    if [[ "zprime_m1000_w10" == "$input" ]]
+    then
+        echo -n "1"
+    elif [[ "zprime_m1500_w15" == "$input" ]]
+    then
+        echo -n "1.5"
+    elif [[ "zprime_m2000_w20" == "$input" ]]
+    then
+        echo -n "2"
+    elif [[ "zprime_m3000_w30" == "$input" ]]
+    then
+        echo -n "3"
+    else
+        echo -n "XYZ"
+    fi
+    echo -n "\\TeVcc\$ & "
+
     sum_samples "$input"
 done
 echo \\hline
 
-echo -n "stop & "
+echo -n "Single top & "
 sum_samples "stop" satop
 
 for input in {w,z,tt}jets
 do
-    echo -n "$input & "
+    if [[ "wjets" == "$input" ]]
+    then
+        echo -n "\$W\$+jets & "
+    elif [[ "zjets" == "$input" ]]
+    then
+        echo -n "\$Z\$+jets & "
+    elif [[ "ttjets" == "$input" ]]
+    then
+        echo -n "\$t\\bar{t}\$ & "
+    else
+        echo -n "$input & "
+    fi
+
     sum_samples "$input"
 done
 
 echo \\hline
 
-echo -n "total & "
+echo -n "Total MC & "
 sum_samples "stop" satop {w,z,tt}jets
 
-echo -n "qcd_data & "
+echo -n "QCD data-driven & "
 sum_samples qcd_data
 
-echo -n "total background & "
+echo \\hline
+
+echo -n "Total background & "
 sum_samples "stop" satop {w,z,tt}jets qcd_data
 
-echo -n "data & "
-sum_samples data
+echo -n "Data 2011 & "
+sum_samples_no_error data
 
 echo \\hline
 
