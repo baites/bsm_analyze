549bb6bde45b215049118d5357ffd3b6e4d01030
Samvel Khalatyan <samvel.khalatyan@gmail.com>
2012-01-25 10:54:45 -0600

    Add ROOT files comparison

--- test/macros/compare.py
+++ test/macros/compare.py
@@ -5,9 +5,10 @@
 
 from __future__ import division, print_function
 
+import os
 import sys
 
-from ROOT import *
+import ROOT
 
 class Compare():
     def __init__(self):
@@ -31,6 +32,10 @@ class Compare():
         self.ratio.SetDirectory(0)
         self.ratio.Reset()
         self.ratio.Divide(self.numerator, self.denominator)
+        self.ratio.GetYaxis().SetTitle("#frac{" + numerator.GetTitle() + "}{"
+                + denominator.GetTitle() + "}")
+        self.ratio.GetYaxis().SetTitleSize(0.08)
+        self.ratio.GetYaxis().SetTitleOffset(0.5)
 
         axis = self.ratio.GetYaxis()
         axis.SetNdivisions(4)
@@ -45,8 +50,9 @@ class Compare():
         self.ratio.SetMarkerSize(0.5)
         self.ratio.SetLineWidth(1)
         self.ratio.SetLineColor(ROOT.kGray + 1)
+        self.ratio.SetMarkerColor(ROOT.kBlack)
 
-        self.legend = TLegend(.8, .3, .9, .5)
+        self.legend = ROOT.TLegend(.6, .3, .9, .5)
         self.legend.SetMargin(0.12)  
         self.legend.SetTextSize(0.03)
         self.legend.SetFillColor(10) 
@@ -59,7 +65,7 @@ class Compare():
 
         # Draw Canvas
         #
-        self.canvas = TCanvas()
+        self.canvas = ROOT.TCanvas()
         self.canvas.SetWindowSize(640, 640)
         self.canvas.Divide(1, 2)
 
@@ -69,7 +75,7 @@ class Compare():
         pad.SetPad(0, 0.3, 1, 1)
         pad.SetRightMargin(5)
 
-        self.stack = THStack()
+        self.stack = ROOT.THStack()
         self.stack.Add(self.numerator)
         self.stack.Add(self.denominator)
 
@@ -87,6 +93,77 @@ class Compare():
         self.ratio.Draw("e 9")
 
         self.canvas.Update()
+
+class CompareFiles():
+    def __init__(self):
+        self.input = None
+        self.compare = []
+
+    def __call__(self, left_input, right_input):
+        if not left_input:
+            raise Exception("left input is not specified")
+
+        if not right_input:
+            raise Exception("right input is not specified")
+
+        if not os.path.lexists(left_input):
+            raise Exception("left input does not exist: " + left_input)
+
+        if not os.path.lexists(right_input):
+            raise Exception("right input does not exist: " + right_input)
+
+        self.input = {
+                "left": ROOT.TFile(left_input),
+                "right": ROOT.TFile(right_input)
+                }
+
+        names = {}
+        for key, file in self.input.items():
+            if file.IsZombie():
+                raise Exception("failed to open file: " + file.GetName())
+
+            names[key] = set([x.GetName() for x in file.GetListOfKeys()])
+
+        common = names["left"] & names["right"]
+        left_only = names["left"] - names["right"]
+        right_only = names["right"] - names["left"]
+
+        if left_only:
+            print("-- Templates found in the file only: {0} ".format(left_input).ljust(80))
+            print(left_only)
+            print()
+
+        if right_only:
+            print("-- Templates found in the file only: {0} ".format(right_input).ljust(80))
+            print(right_only)
+            print()
+
+        for name in common:
+            lh = self.input["left"].Get(name)
+            if not lh:
+                print("faile to read template: " + name, file = sys.stderr)
+
+                continue
+
+            lh = lh.Clone()
+            lh.SetLineColor(ROOT.kRed + 1)
+            lh.SetFillColor(0)
+            lh.SetTitle(os.path.basename(left_input).rsplit(".", 1)[0])
+
+            rh = self.input["right"].Get(name)
+            if not rh:
+                print("faile to read template: " + name, file = sys.stderr)
+
+                continue
+
+            rh = rh.Clone()
+            rh.SetLineColor(ROOT.kGreen + 1)
+            rh.SetFillColor(0)
+            rh.SetTitle(os.path.basename(right_input).rsplit(".", 1)[0])
+
+            compare = Compare()
+            compare(numerator = lh, denominator = rh, title = name)
+            self.compare.append(compare)
 
 if "__main__" == __name__:
     print("module is not designed for execution", file = sys.stderr)
