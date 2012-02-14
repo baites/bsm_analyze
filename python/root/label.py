3c3060964badef9f249c85bbfcea875211050306
Samvel Khalatyan <samvel.khalatyan@gmail.com>
2012-02-14 15:49:00 -0600

    Document Label class

--- python/root/label.py
+++ python/root/label.py
@@ -10,6 +10,15 @@ from __future__ import print_function, division
 import ROOT
 
 class Label(object):
+    '''
+    The most generic legend by managing label proprty, e.g.:
+
+        my_label = Label()
+        my_label.label = "This is a generic label"
+        ...
+        my_label.Draw()
+    '''
+
     def __init__(self):
         self._label = None
 
@@ -19,6 +28,11 @@ class Label(object):
 
     @label.setter
     def label(self, header):
+        '''
+        Set label header. Label object will be automatically created if it
+        does not exist yet
+        '''
+
         if self:
             self.label.SetHeader(header)
         else:
@@ -29,6 +43,11 @@ class Label(object):
             self.label.SetTextAlign(32)
             self.label.SetTextSize(0.12)
 
+            '''
+            recursion to absorb Set Header procedure if it is changed in future,
+            for example if some logic or setup is added
+            '''
+
             self.label = header
 
     @label.deleter
@@ -36,18 +55,30 @@ class Label(object):
         del self._label
 
     def __nonzero__(self):
+        '''
+        Python 2.x hook
+        '''
+
         return self.__bool__()
 
     def __bool__(self):
         return bool(self.label)
 
     def __str__(self):
+        '''
+        Nice print for label with class name and header if any is set
+        '''
+
         return "<{klass} {header!r} at 0x{id:x}>".format(
                 klass = self.__class__.__name__,
                 header = self.label.GetHeader() if self else "",
                 id = id(self))
 
 class CMSLabel(Label):
+    '''
+    CMS Label with experiment energy, specific size and location, etc.
+    '''
+
     def __init__(self):
         Label.__init__(self)
         self.label = "CMS Preliminary #sqrt{s} = 7 TeV"
@@ -59,6 +90,10 @@ class CMSLabel(Label):
         self.label.SetY2(.96)
 
 class LuminosityLabel(Label):
+    '''
+    Luminosity label
+    '''
+
     def __init__(self, luminosity):
         Label.__init__(self)
         self.label = ("L = {0:.2f}".format(luminosity / 1000) +
