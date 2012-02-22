aaa87035cad14711d6c900f0f3bfb007aec5ba8d
Samvel Khalatyan <samvel.khalatyan@gmail.com>
2012-02-21 21:13:42 -0600

    Expand examples and tests

--- python/template/input.py
+++ python/template/input.py
@@ -85,34 +85,82 @@ if "__main__" == __name__:
         - print input with scale factors, etc.
     '''
 
-    # Prepare function for later random fill
-    my_gaus = ROOT.TF1("my_gaus", "gaus(0)", 0, 100)
-    my_gaus.SetParameters(1, 50, 10)
+    try:
+        print("{0:-<80}".format("-- Create plot, random fill, scale, draw "))
 
-    # Create plot and randomly fill with above function
-    plot = ROOT.TH1F("plot", "plot", 100, 0, 100);
-    plot.FillRandom("my_gaus", 10000)
+        # Prepare function for later random fill
+        my_gaus = ROOT.TF1("my_gaus", "gaus(0)", 0, 100)
+        my_gaus.SetParameters(1, 50, 10)
 
-    # Create random input
-    import random
-    input_ = Input(random.choice(InputType.input_types.keys()), plot)
+        # Create plot and randomly fill with above function
+        plot = ROOT.TH1F("plot", "plot", 100, 0, 100);
+        plot.FillRandom("my_gaus", 10000)
 
-    # Prepare canvas for drawing
-    canvas = ROOT.TCanvas()
-    canvas.SetWindowSize(800, 480)
-    canvas.Divide(2)
+        # Create random input
+        import random
+        input_ = Input(random.choice(InputType.input_types.keys()), plot)
 
-    # Draw original plot
-    canvas.cd(1)
-    plot.Draw("hist 9")
+        # Prepare canvas for drawing
+        canvas = ROOT.TCanvas()
+        canvas.SetWindowSize(800, 480)
+        canvas.Divide(2)
 
-    # Draw input plot (it should be scaled)
-    canvas.cd(2)
-    input_.plot.Draw("hist 9")
+        # Draw original plot
+        canvas.cd(1)
+        plot.Draw("hist 9")
 
-    canvas.Update()
+        # Draw input plot (it should be scaled)
+        canvas.cd(2)
+        input_.plot.Draw("hist 9")
 
-    # print input
-    print(input_)
+        canvas.Update()
 
-    raw_input("enter")
+        # print input
+        print(input_)
+
+        raw_input("enter")
+
+    except AttributeError as error:
+        print(error)
+    finally:
+        print('-' * 80)
+
+    try:
+        print("{0:-<80}".format("-- Expand Input types with QCD, Create plot, "
+                                "random fill, scale, draw "))
+
+        from input_type import InputData
+
+        class InputWithQCD(Input):
+            input_types = Input.input_types.copy()
+            input_types.update({
+                "qcd": InputData(2.361e8 * 1.06e-2, 35729669)
+            })
+
+        # Prepare canvas for drawing
+        canvas2 = ROOT.TCanvas()
+        canvas2.SetWindowSize(800, 480)
+        canvas2.Divide(2)
+
+        # Create and draw qcd
+        canvas2.cd(1)
+        qcd = InputWithQCD("qcd", plot)
+        qcd.plot.Draw("hist 9")
+
+        # Create and draw ttbar
+        canvas2.cd(2)
+        ttbar = InputWithQCD("ttbar", plot)
+        ttbar.plot.Draw("hist 9")
+
+        canvas2.Update()
+
+        # print input
+        print(qcd)
+        print(ttbar)
+
+        raw_input("enter")
+
+    except AttributeError as error:
+        print(error)
+    finally:
+        print('-' * 80)
