d444d93dbba096c73d32424e2744fa73ff151866
Samvel Khalatyan <samvel.khalatyan@gmail.com>
2012-02-29 09:45:39 -0600

    Move all code from main to templates, support args

--- python/template/main.py
+++ python/template/main.py
@@ -7,10 +7,8 @@ Copyright 2011, All rights reserved
 
 from __future__ import print_function
 
-import root.style
 import sys
 import templates
-
 
 def usage(argv):
     return ("usage: {0} [plots:A,B,C] [folders:A,B,C] "
@@ -18,24 +16,8 @@ def usage(argv):
 
 def main(argv = sys.argv):
     try:
-        style = root.style.tdr()
-        style.cd()
-
-        # Don't forget to add plots, folders and channels support to main.py
-
         app = templates.Templates(verbose = True)
-        app.folders_to_use = [""]
-        app.plots_to_use = ["mttbar_after_htlep"]
-        app.run([
-            "qcd", "data",
-
-            "ttbar", "zjets", "wjets", "stop",
-
-            "zprime_m1000_w10",
-            "zprime_m1500_w15",
-            "zprime_m2000_w20",
-            "zprime_m3000_w30",
-                  ])
+        app.run(argv[1:])
 
     except Exception as error:
         # print Exception traceback for debug
--- python/template/templates.py
+++ python/template/templates.py
@@ -7,19 +7,73 @@ Copyright 2011, All rights reserved
 
 from __future__ import print_function
 
+import channel_type
+import root.style
+
 from loader import ChannelTemplateLoader
+from util.arg import split_use_and_ban
 
 class Templates(object):
     def __init__(self, verbose = False):
-        self.plots_to_use = []
-        self.folders_to_use = []
+        self.use_plots = []
+        self.ban_plots = []
+
+        self.use_folders = []
+        self.ban_folders = []
+
+        self.channels = []
+
         self.__verbose = verbose
 
-    def run(self, channels):
+    def run(self, args):
+        args = [x.split(':') for x in args if ':' in x]
+        args = {key.strip():
+                    set(x.strip() for x in values.split(','))
+                        for key, values in args}
+
+        self.use_plots, self.ban_plots = map(list,
+                                             split_use_and_ban(args.get("plots",
+                                                                        [])))
+
+        use_folders, ban_folders = map(list,
+                                       split_use_and_ban(args.get("folders",
+                                                                  [])))
+
+        use_channels, ban_channels = split_use_and_ban(args.get("channels",
+                                                                []))
+
+        channels = set(channel_type.ChannelType.channel_types.keys())
+        if use_channels:
+            channels &= use_channels
+
+        if ban_channels:
+            channels -= ban_channels
+
+        self.channels = list(channels)
+
+        if self.__verbose:
+            print("{0:-<80}".format("-- Configuration "))
+            print(self)
+            print()
+
+        self.process()
+
+    def process(self):
+        if not self.channels:
+            return
+
+        style = root.style.tdr()
+        style.cd()
+
         loader = ChannelTemplateLoader("output_signal_p150_hlt.root")
-        loader.use_folders.extend(self.folders_to_use)
-        loader.use_plots.extend(self.plots_to_use)
-        loader.load(channels)
+
+        loader.use_plots = self.use_plots
+        loader.ban_plots = self.ban_plots
+
+        loader.use_folders = self.use_folders
+        loader.ban_folders = self.ban_folders
+
+        loader.load(self.channels)
 
         if self.__verbose:
             print(loader)
@@ -28,4 +82,15 @@ class Templates(object):
 
         # Plot histograms
 
-        # Don't forget to add plots, folders and channels support to main.py
+    def __str__(self):
+        result = []
+
+        result.append("  use plots: {0}".format(self.use_plots))
+        result.append("  ban plots: {0}".format(self.ban_plots))
+
+        result.append("use folders: {0}".format(self.use_folders))
+        result.append("ban folders: {0}".format(self.ban_folders))
+
+        result.append("   channels: {0}".format(self.channels))
+
+        return '\n'.join(result)
