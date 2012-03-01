477d20b526f20b314a59cfb0e13bc25a96cdcbeb
Samvel Khalatyan <samvel.khalatyan@gmail.com>
2012-02-29 23:01:43 -0600

    Adjust top/bottom TDR styles

--- python/root/style.py
+++ python/root/style.py
@@ -72,8 +72,10 @@ def tdr():
     style.SetStatW(0.4)
 
     # Margins:
+    '''
     style.SetPadTopMargin(10)
     style.SetPadBottomMargin(0.15)
+    '''
     style.SetPadLeftMargin(0.2)
     style.SetPadRightMargin(10)
 
