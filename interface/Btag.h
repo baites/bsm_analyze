a412f16702cba524d7898724ba26572318189838
Samvel Khalatyan <samvel.khalatyan@gmail.com>
2012-03-28 18:27:44 +0200

    Implement Btag class with options

--- interface/Btag.h
+++ interface/Btag.h
@@ -10,13 +10,53 @@
 
 #include "bsm_core/interface/Object.h"
 #include "interface/bsm_fwd.h"
+#include "interface/AppController.h"
+#include "interface/DelegateManager.h"
 
 namespace bsm
 {
-    class Btag: public core::Object
+    class BtagDelegate
+    {
+        public:
+            enum Systematic
+            {
+                DOWN = -1,
+                NONE = 0,
+                UP = 1
+            };
+
+            virtual ~BtagDelegate()
+            {
+            }
+
+            virtual void setSystematic(const Systematic &)
+            {
+            }
+    };
+
+    class BtagOptions:
+        public Options,
+        public DelegateManager<BtagDelegate>
+    {
+        public:
+            BtagOptions();
+
+            // Options interface
+            //
+            virtual DescriptionPtr description() const;
+
+        private:
+            void setSystematic(const BtagDelegate::Systematic &);
+
+            DescriptionPtr _description;
+    };
+
+    class Btag: public core::Object,
+                public BtagDelegate
     {
         public:
             Btag();
+            Btag(const Btag &);
 
             static float discriminator();
 
@@ -25,8 +65,18 @@ namespace bsm
 
             static float mistag_efficiency(const float &jet_pt);
             static float mistag_scale(const float &jet_pt);
+            static float mistag_scale_sigma_up(const float &jet_pt);
+            static float mistag_scale_sigma_down(const float &jet_pt);
+
+            float btag_scale_with_systematic(const float &discriminator,
+                                             const float &uncertainty);
+            float mistag_scale_with_systematic(const float &jet_pt);
 
             bool is_tagged(const CorrectedJet *jet);
+
+            // BtagDelegate interface
+            //
+            virtual void setSystematic(const Systematic &);
 
             // Object interface
             //
@@ -35,11 +85,17 @@ namespace bsm
             virtual void print(std::ostream &) const;
 
         private:
+            // Prevent copying
+            //
+            Btag &operator =(const Btag &);
+
             bool correct(const bool &is_tagged,
                          const float &scale,
                          const float &efficiency);
 
             boost::shared_ptr<boost::mt19937> _generator;
+
+            Systematic _systematic;
     };
 }
 
--- src/Btag.cc
+++ src/Btag.cc
@@ -3,16 +3,65 @@
 // Created by Samvel Khalatyan, Mar 25, 2011
 // Copyright 2011, All rights reserved
 
+#include <stdexcept>
+
 #include "bsm_core/interface/ID.h"
+#include "bsm_input/interface/Algebra.h"
 #include "bsm_input/interface/Jet.pb.h"
 
 #include "interface/Btag.h"
 #include "interface/CorrectedJet.h"
 
 using namespace bsm;
+using namespace std;
 
-Btag::Btag()
+BtagOptions::BtagOptions()
 {
+    _description.reset(new po::options_description("Btag Options"));
+    _description->add_options()
+        ("btag-up",
+         po::value<bool>()->notifier(
+             boost::bind(&BtagOptions::setSystematic, this, BtagDelegate::UP)),
+         "Change btag SF one sigma up")
+
+        ("btag-down",
+         po::value<bool>()->notifier(
+             boost::bind(&BtagOptions::setSystematic, this, BtagDelegate::DOWN)),
+         "Change btag SF one sigma down")
+    ;
+}
+
+// Options interface
+//
+BtagOptions::DescriptionPtr BtagOptions::description() const
+{
+    return _description;
+}
+
+// Private
+//
+void BtagOptions::setSystematic(const BtagDelegate::Systematic &systematic)
+{
+    if (!delegate())
+        return;
+
+    delegate()->setSystematic(systematic);
+}
+
+
+
+// Btag
+//
+Btag::Btag():
+    _systematic(NONE)
+{
+    _generator.reset(new boost::mt19937());
+}
+
+Btag::Btag(const Btag &object):
+    _systematic(object._systematic)
+{
+    _generator.reset(new boost::mt19937());
 }
 
 float Btag::discriminator()
@@ -50,6 +99,45 @@ float Btag::mistag_scale(const float &jet_pt)
            5.50157e-09 * pow(jet_pt, 3);
 }
 
+float Btag::mistag_scale_sigma_down(const float &jet_pt)
+{
+    return 0.899715 +
+           0.00102278 * jet_pt -
+           2.46335e-06 * pow(jet_pt, 2) +
+           9.71143e-10 * pow(jet_pt, 3);
+}
+
+float Btag::mistag_scale_sigma_up(const float &jet_pt)
+{
+    return 0.997077 +
+           0.00473953 * jet_pt -
+           1.34985e-05 * pow(jet_pt, 2) +
+           1.0032e-08 * pow(jet_pt, 3);
+}
+
+float Btag::btag_scale_with_systematic(const float &discriminator,
+                                       const float &uncertainty)
+{
+    float scale = Btag::btag_scale(discriminator);
+    
+    if (_systematic)
+        scale += _systematic * uncertainty;
+
+    return scale;
+}
+
+float Btag::mistag_scale_with_systematic(const float &jet_pt)
+{
+    switch(_systematic)
+    {
+        case DOWN: return Btag::mistag_scale_sigma_down(jet_pt);
+        case NONE: return Btag::mistag_scale(jet_pt);
+        case UP: return Btag::mistag_scale_sigma_up(jet_pt);
+
+        default: throw runtime_error("unsupported systematic type used");
+    }
+}
+
 bool Btag::is_tagged(const CorrectedJet *jet)
 {
     typedef ::google::protobuf::RepeatedPtrField<Jet::BTag> BTags;
@@ -60,28 +148,45 @@ bool Btag::is_tagged(const CorrectedJet *jet)
     {
         if (Jet::BTag::CSV == btag->type())
         {
-            bool result = Btag::discriminator() < btag->discriminator();
+            const float discriminator = btag->discriminator();
+            bool result = Btag::discriminator() < discriminator;
 
             if (jet->jet->has_gen_parton())
             {
+                float scale = 0;
+                float efficiency = 0;
+
                 switch(abs(jet->jet->gen_parton().id()))
                 {
-                    case 5: // fall through
+                    case 5:
+                        scale = btag_scale_with_systematic(discriminator, 0.04);
+                        efficiency = Btag::btag_efficiency(discriminator);
+
+                        break;
+
                     case 4:
-                        result = correct(
-                            result,
-                            Btag::btag_scale(Btag::discriminator()),
-                            Btag::btag_efficiency(Btag::discriminator()));
+                        scale = btag_scale_with_systematic(discriminator, 0.08);
+                        efficiency = Btag::btag_efficiency(discriminator);
+
                         break;
 
                     case 3: // fall through
                     case 2: // fall through
                     case 1:
-                        const float jet_pt = 0;
-                        result = correct(result,
-                                         Btag::mistag_scale(jet_pt),
-                                         Btag::mistag_efficiency(jet_pt));
+                        const float jet_pt = pt(*jet->corrected_p4);
+
+                        scale = mistag_scale_with_systematic(jet_pt);
+                        efficiency = Btag::mistag_efficiency(jet_pt);
+
                         break;
+                }
+
+                if (scale && efficiency)
+                {
+                    _generator->seed(static_cast<unsigned int>(
+                                        sin(phi(*jet->corrected_p4) * 1000000)));
+
+                    result = correct(result, scale, efficiency);
                 }
             }
 
@@ -90,6 +195,13 @@ bool Btag::is_tagged(const CorrectedJet *jet)
     }
 
     return false;
+}
+
+// BtagDelegate interface
+//
+void Btag::setSystematic(const Systematic &systematic)
+{
+    _systematic = systematic;
 }
 
 // Object interface
@@ -120,19 +232,18 @@ bool Btag::correct(const bool &is_tagged,
         return is_tagged;
 
     bool result = is_tagged;
-    float random_value = 0;
     if (1 < scale)
     {
         if (!is_tagged)
         {
-            if (random_value < (1 - scale) / (1 - scale / efficiency))
+            if ((*_generator)() < (1 - scale) / (1 - scale / efficiency))
                 result = true;
         }
     }
     else
     {
         if (is_tagged &&
-            random_value > scale)
+            (*_generator)() > scale)
 
             result = false;
     }
